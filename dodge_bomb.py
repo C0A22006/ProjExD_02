5b4d7052195d9b7878a44c02a8333286d05bfb59
arita <c0a22006b2@edu.teu.ac.jp>
2023-04-25 14:20:45 +0900

    lec2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,6 @@
+import random
 import pygame as pg
+
 import sys
 
 def main():
@@ -8,10 +10,11 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20,20)) # 練習１
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10) # 練習１
+    bb_img.set_colorkey((0,0,0)) # 練習１
+    x, y = random.randint(0,1600),random.randint(0,900) # 練習2
     tmr = 0
-    bb_img = pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
 
     while True:
         for event in pg.event.get():
@@ -21,7 +24,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        
+        screen.blit(bb_img, [x,y])  # 練習2
 
         pg.display.update()
         clock.tick(1000)
